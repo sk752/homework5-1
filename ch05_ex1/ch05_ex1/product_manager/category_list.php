6b382e00aae27da14ff15806119274a5a2310646
sravani kanchanapalli <sk752@afsconnect1.njit.edu>
2017-04-23 22:00:34 -0400

    added category list

--- ch05_ex1/ch05_ex1/product_manager/category_list.php
+++ ch05_ex1/ch05_ex1/product_manager/category_list.php
@@ -7,7 +7,20 @@
             <th>Name</th>
             <th>&nbsp;</th>
         </tr>        
-        <!-- add category rows here -->
+        <?php foreach ($categories as $category): ?>
+	<tr>
+	  <td> <?php echo $category ['categoryName']; ?> </td>
+	  <td>
+	    <form action = "index.php" method ="post">
+	      <input type = "hidden" name ="action" value = "delete_category"
+	      />
+	      <input type="hidden" name="category_id"
+	      	value="<?php echo $category['categoryID']; ?>" />
+	      <input type ="submit" value="delete"/>
+	    </form>
+	  </td>
+	</tr>
+	<?php endforeach; ?>
     </table>
 
     <h2>Add Category</h2>
